71f689263a00279942d2f5ca77a390a866ffb111
lachgar <lachgar.m@gmail.com>
2023-11-04 15:31:41 +0100

    first commit

---
+++ src/main/java/ma/projet/entities/Student.java
@@ -0,0 +1,46 @@
+package ma.projet.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Student extends User {
+	
+	private String firstName;
+	private String lastName;
+	private String telephone;
+	@ManyToOne
+	private Filiere filiere;
+	
+	public Student() {
+		super();
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public Filiere getFiliere() {
+		return filiere;
+	}
+	public void setFiliere(Filiere filiere) {
+		this.filiere = filiere;
+	}
+	
+	
+	
+
+}
